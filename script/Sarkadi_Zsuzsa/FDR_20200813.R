3cc2df81b803baef34d20bb1f8cdf10f77dcdbd9
zsarkadi <zsarkadi@gmail.com>
2020-08-13 15:43:49 +0200

    első rész: random számok generálása, t-test

---
+++ script/Sarkadi_Zsuzsa/FDR_20200813.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+#Normális eloszlású random számot az rnorm() függvénnyel tudtok generálni
+#A kezeletlen esetben az OD  várható értéke 1.2 és a méréshez tartozó szórás 0.25
+#3-3 szimulált mintát
+kezeletlen <- rnorm(3, mean=1.2, sd=0.25)
+
+#AMP kezelés mellett az OD várható értéke 1.1 és a hozzá tartozó szórás 0.4
+kezeltAMP <- rnorm(3, mean=1.1, sd=0.4)
+
+t.test(kezeletlen, kezeltAMP)
+
