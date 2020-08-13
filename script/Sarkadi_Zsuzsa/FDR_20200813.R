6591971751408b5c50b61e8410abd2fb84e2b89c
zsarkadi <zsarkadi@gmail.com>
2020-08-13 17:58:56 +0200

    még kell rajta dolgoznom

--- script/Sarkadi_Zsuzsa/FDR_20200813.R
+++ script/Sarkadi_Zsuzsa/FDR_20200813.R
@@ -23,3 +23,17 @@ kezeltAMP <- rnorm(15, mean=1.2, sd=0.25)
 t.test(kezeletlen, kezeltAMP)
 
 
+#Alfeladat3: N=5000  t-testet úgy hogy mindig vesztek 3-3 NT és AMP mintát, mindegyikre kiszámoljátok a p-értéket. Az eredményeket tegyétek be egy N soros táblázatba . 
+kezeletlen <- rnorm(3, mean=1.2, sd=0.25)
+kezeltAMP <- rnorm(3, mean=1.1, sd=0.4)
+
+tbl1 <- tibble(experiment_id=character(), p=numeric(), type=character())
+
+
+for(i in 1:5000) {
+  sprintf("exp_%03d",tbl1[i] )
+}
+i
+p=t.test(kezeletlen, kezeltAMP)$p.value    
+sprintf("exp_%03d",i )
+
